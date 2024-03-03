c0b7c5f16302e84322def90b8e1fa62001145ddc
Mateus Sousa <112879906+mateus-s-silva@users.noreply.github.com>
2024-03-03 15:47:33 -0300

    Time: 0 ms (100.00%), Space: 41.7 MB (94.43%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,20 +1,28 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
     
-        int lastNums1 = m - 1;
-        int lastNums2 = n - 1;
-        int realLastNums1 = m + n - 1;
+        // array nums1 and array nums2 in non-decreasing orde
+        // m is nums1.size and n is nums2.size
+        // the return must be array nums1 in crescent order
+        // nums1 has a lenght of m+n
         
-        while (lastNums2 >= 0) {
-            if (lastNums1 >= 0 && nums1[lastNums1] > nums2[lastNums2]) {
+        
+        var lastNums1 = m - 1;
+        var lastNums2 = n - 1;
+        var realLastNums1 = m + n - 1;
+        
+        
+        while(lastNums2 >= 0){
+            if(lastNums1 >= 0 && nums1[lastNums1] >= nums2[lastNums2]){
                 nums1[realLastNums1] = nums1[lastNums1];
-                realLastNums1--;
                 lastNums1--;
-            } else {
-                nums1[realLastNums1] = nums2[lastNums2];
                 realLastNums1--;
-                lastNums2--;
             }
-        }
+            else{
+                nums1[realLastNums1] = nums2[lastNums2];
+                lastNums2--;
+                realLastNums1--;
+            }
+        } 
     }
 }
