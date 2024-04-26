e6999599c127a1f61305c9fb645c0508f73920b3
Mateus Sousa <112879906+mateus-s-silva@users.noreply.github.com>
2024-04-26 15:22:12 -0300

    Time: 0 ms (100.00%), Space: 41.6 MB (73.33%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,10 +3,6 @@ class Solution {
         int sizeArray = nums.length - 1;
         int aux, idx = 0;
         
-        // [3(idx), 2, 2, 3(sizeArray-1)]  
-        // primeira interação: [3(idx), 2, 2(sizeArray), 3]
-        // segunda interação: [2, 2(sizeArray, idx), 3, 3]
-        // terceira interação: 
         while(idx <= sizeArray){
             if(nums[idx] == val){
                 aux = nums[sizeArray];
